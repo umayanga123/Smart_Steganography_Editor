6fca7e3ffd2ce91ec36c022768aca23e27f9e96f
umayanga <uma.madhushan@outlook.com>
2019-08-26 23:07:46 +0530

    add ok butten and action

--- EncorderComponent/EncoderForm.cpp
+++ EncorderComponent/EncoderForm.cpp
@@ -1,2 +1,20 @@
 #include "EncoderForm.h"
+#include <windows.h>
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+using namespace System::Diagnostics;
+
+
+[STAThread]
+void main(array<String^>^ arg) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	EncorderComponent::EncoderForm form;
+	Application::Run(%form);
+
+}
+
 
--- EncorderComponent/EncoderForm.h
+++ EncorderComponent/EncoderForm.h
@@ -34,6 +34,9 @@ namespace EncorderComponent {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Button^ button1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +51,48 @@ namespace EncorderComponent {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"EncoderForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(88, 31);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->TabIndex = 0;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(173, 204);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"ok";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &EncoderForm::Button1_Click);
+			// 
+			// EncoderForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedToolWindow;
+			this->Name = L"EncoderForm";
+			this->Text = L"Encoder Form";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
+	private: System::Void Button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		String^ message = "Hello ";
+		String^ title = "Welcome";
+		Char a = '!'; //A managed character
+		MessageBox::Show(message + textBox1->Text + a, title, MessageBoxButtons::OK);
+
+	}
 	};
 }
