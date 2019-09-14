2c193d09ac779718cf72d4b165056679826b6a87
umayanga <uma.madhushan@outlook.com>
2019-09-14 22:30:20 +0530

    clean code and add file browsing

--- EncorderComponent/EncoderAlgo.cpp
+++ EncorderComponent/EncoderAlgo.cpp
@@ -1,42 +1,25 @@
 #include <opencv2/opencv.hpp>
-
-class Test {
-public:
-	int A() {
-		cv::Mat img = cv::imread("lena.png");
-		cv::namedWindow("image", cv::WindowFlags::WINDOW_NORMAL);
-		imshow("image", img);
-		return 0;
-	}
+#include <stdlib.h>
+class EncorderAlgo{
 
 public:
-	int LSB_encoder(std::string text) {
+	int LSB_encoder(std::string text ,std::string path) {
 
 		// Stores original image
-		cv::Mat image = cv::imread("lena.png");
+		cv::Mat image = cv::imread(path);
 		if (image.empty()) {
-			std::cout << "Image Error\n";
-			exit(-1);
+			return -1;
 		}
 
-		// Open file for text information
-		std::fstream file;
-		file.open("example.txt");
-		if (!file.is_open()) {
-			std::cout << "File Error\n";
-			exit(-1);
-		}
-
+		// secrect text information
 		std::string word = text;
 		if (word.length() < 1) {
-			std::cout << "File Error\n";
-			exit(-1);
+			return -2;
 		}
 
 		// char to work on
 		char ch;
 		// reads the first char from the file
-		//file.get(ch);
 		int i = 0;
 		ch = word.at(i);
 		i++;
@@ -77,15 +60,13 @@ public:
 					// if last_null_char is true and bit_count is 8, then our message is successfully encode.
 					if (last_null_char && bit_count == 8) {
 						encoded = true;
-						goto OUT;
+						break;
 					}
 
 					// if bit_count is 8 we pick the next char from the file and work on it
 					if (bit_count == 8) {
 						bit_count = 0;
-						//file.get(ch);
-						
-						
+				
 						if (word.length() > i) {
 							ch = word.at(i);
 							i++;
@@ -94,95 +75,28 @@ public:
 							last_null_char = true;
 							ch = '\0';
 						}
-
-						// if EndOfFile(EOF) is encountered insert NULL char to the image
-						/*if (file.eof()) {
-							last_null_char = true;
-							ch = '\0';
-						}*/
 					}
 
 				}
 			}
-		}
-	OUT:;
+		};
 
 		// whole message was not encoded
 		if (!encoded) {
-			std::cout << "Message too big. Try with larger image.\n";
-			exit(-1);
+			return -3;
 		}
 
 		// Writes the stegnographic image
-		imwrite("output_image.png", image);
+		imwrite("out_"+ path.substr(path.find_last_of("/\\") + 1), image);
 
 		return 0;
 	}
 
 
-public:
-	std::string  LSB_decoder() {
-		// Stores original image
-		cv::Mat image = cv::imread("output_image.png");
-		if (image.empty()) {
-			std::cout << "Image Error\n";
-			exit(-1);
-		}
-       
-		std::string msg;
-		// char to work on
-		char ch = 0;
-		// contains information about which bit of char to work on
-		int bit_count = 0;
-
-		/*
-		To extract the message from the image, we will iterate through the pixels and extract the LSB of
-		the pixel values (RGB) and this way we can get our message.
-		*/
-		for (int row = 0; row < image.rows; row++) {
-			for (int col = 0; col < image.cols; col++) {
-				for (int color = 0; color < 3; color++) {
-
-					// stores the pixel details
-					cv::Vec3b pixel = image.at<cv::Vec3b>(cv::Point(row, col));
-
-					// manipulate char bits according to the LSB of pixel values
-					if (isBitSet(pixel.val[color], 0))
-						ch |= 1;
-
-					// increment bit_count to work on next bit
-					bit_count++;
-
-					// bit_count is 8, that means we got our char from the encoded image
-					if (bit_count == 8) {
-
-						// NULL char is encountered
-						if (ch == '\0')
-							goto OUT;
-
-						bit_count = 0;
-						msg +=ch;
-						//std::cout << ch;
-						ch = 0;
-					}
-					else {
-						ch = ch << 1;
-					}
-
-				}
-			}
-		}
-	OUT:;
-
-
-		return msg;
-	}
-
-
 private:
 	// Checks whether the bit is set or not at a particular position.
-    // Returns true if set
-    // Returns false if not set
+	// Returns true if set
+	// Returns false if not set
 	bool isBitSet(char ch, int pos) {
 		// 7 6 5 4 3 2 1 0
 		ch = ch >> pos;
--- EncorderComponent/MainForm.h
+++ EncorderComponent/MainForm.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "ImageTest.cpp"
+#include "msclr\marshal_cppstd.h"
+#include "EncoderAlgo.cpp"
+#include "DecoderAlgo.cpp"
 
 namespace MainComponent {
 
@@ -10,19 +13,12 @@ namespace MainComponent {
 	using namespace System::Data;
 	using namespace System::Drawing;
 	
-
-	/// <summary>
-	/// Summary for EncoderForm
-	/// </summary>
 	public ref class MainForm : public System::Windows::Forms::Form
 	{
 	public:
 		MainForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
 		}
 
 	protected:
@@ -42,13 +38,16 @@ namespace MainComponent {
 	private: System::Windows::Forms::Button^ button3;
 	private: System::Windows::Forms::Button^ button4;
 	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::OpenFileDialog^ openFileDialog;
+	private: System::ComponentModel::IContainer^ components;
 	protected:
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -63,18 +62,20 @@ namespace MainComponent {
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->button4 = (gcnew System::Windows::Forms::Button());
 			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->openFileDialog = (gcnew System::Windows::Forms::OpenFileDialog());
 			this->SuspendLayout();
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(88, 31);
+			this->textBox1->Location = System::Drawing::Point(126, 31);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->Size = System::Drawing::Size(210, 20);
 			this->textBox1->TabIndex = 0;
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(173, 110);
+			this->button1->Location = System::Drawing::Point(36, 324);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(75, 23);
 			this->button1->TabIndex = 1;
@@ -84,17 +85,17 @@ namespace MainComponent {
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(33, 110);
+			this->button2->Location = System::Drawing::Point(186, 125);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->Size = System::Drawing::Size(150, 23);
 			this->button2->TabIndex = 2;
-			this->button2->Text = L"Test";
+			this->button2->Text = L"View Image";
 			this->button2->UseVisualStyleBackColor = true;
 			this->button2->Click += gcnew System::EventHandler(this, &MainForm::Button2_Click);
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(173, 226);
+			this->button3->Location = System::Drawing::Point(261, 324);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(75, 23);
 			this->button3->TabIndex = 3;
@@ -104,9 +105,9 @@ namespace MainComponent {
 			// 
 			// button4
 			// 
-			this->button4->Location = System::Drawing::Point(33, 176);
+			this->button4->Location = System::Drawing::Point(186, 67);
 			this->button4->Name = L"button4";
-			this->button4->Size = System::Drawing::Size(75, 23);
+			this->button4->Size = System::Drawing::Size(150, 23);
 			this->button4->TabIndex = 4;
 			this->button4->Text = L"Encode";
 			this->button4->UseVisualStyleBackColor = true;
@@ -114,19 +115,34 @@ namespace MainComponent {
 			// 
 			// button5
 			// 
-			this->button5->Location = System::Drawing::Point(173, 176);
+			this->button5->Location = System::Drawing::Point(186, 96);
 			this->button5->Name = L"button5";
-			this->button5->Size = System::Drawing::Size(75, 23);
+			this->button5->Size = System::Drawing::Size(150, 23);
 			this->button5->TabIndex = 5;
 			this->button5->Text = L"Decode";
 			this->button5->UseVisualStyleBackColor = true;
 			this->button5->Click += gcnew System::EventHandler(this, &MainForm::Button5_Click);
 			// 
-			// EncoderForm
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(12, 34);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(99, 13);
+			this->label1->TabIndex = 6;
+			this->label1->Text = L"Enter Secrect Text:";
+			// 
+			// openFileDialog
+			// 
+			this->openFileDialog->FileName = L"openFileDialog";
+			this->openFileDialog->Title = L"select image";
+			// 
+			// MainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
+			this->ClientSize = System::Drawing::Size(364, 359);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button5);
 			this->Controls->Add(this->button4);
 			this->Controls->Add(this->button3);
@@ -134,8 +150,8 @@ namespace MainComponent {
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->textBox1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedToolWindow;
-			this->Name = L"EncoderForm";
-			this->Text = L"Encoder Form";
+			this->Name = L"MainForm";
+			this->Text = L"Steganography Application";
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -155,13 +171,55 @@ namespace MainComponent {
 	private: System::Void Button3_Click(System::Object^ sender, System::EventArgs^ e) {
 		_Exit(10);
 	}
+
+
 	private: System::Void Button4_Click(System::Object^ sender, System::EventArgs^ e) {
-		Test test;
-		test.LSB_encoder();
+		String^ path;
+		if (openFileDialog->ShowDialog() == System::Windows::Forms::DialogResult::OK) {
+			if (openFileDialog->OpenFile() != nullptr) {
+				path = openFileDialog->InitialDirectory + openFileDialog->FileName;
+				//MessageBox::Show("Secret Message :" + path, "title", MessageBoxButtons::OK);
+			}
+		}
+
+		if (path == nullptr) {
+			return;
+		}
+		std::string new_path = msclr::interop::marshal_as<std::string>(path);
+		System::String^ name = textBox1->Text;
+		if (name->Length < 1) {
+			MessageBox::Show("please enter secrect text", "title", MessageBoxButtons::OK);
+			return;
+		}
+
+		std::string s_text = msclr::interop::marshal_as<std::string>(textBox1->Text);
+		EncorderAlgo encorder;
+		int i = encorder.LSB_encoder(s_text , new_path);
+		if (i == 0) {
+			MessageBox::Show("sucssfully encorded image", "title", MessageBoxButtons::OK);
+		}
+		else {
+			MessageBox::Show("error code :" +i);
+		}
 	}
 	private: System::Void Button5_Click(System::Object^ sender, System::EventArgs^ e) {
-		Test test;
-		std::string msg =test.LSB_decoder();
+		DecorderAlgo decorder;
+		String^ path;
+
+		if (openFileDialog->ShowDialog() == System::Windows::Forms::DialogResult::OK) {
+			if (openFileDialog->OpenFile() != nullptr) {
+				path = openFileDialog->InitialDirectory + openFileDialog->FileName;
+			}
+		}
+
+		if (path == nullptr) {
+
+			return;
+		}
+
+	
+		std::string output_path = msclr::interop::marshal_as<std::string>(path);
+		std::string msg = decorder.LSB_decoder(output_path);
 		String^ str2 = gcnew String(msg.c_str());
 		MessageBox::Show("Secret Message :" + str2, "title", MessageBoxButtons::OK);
 	}
